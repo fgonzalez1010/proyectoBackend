87cac34432f81addd4b2131d03fdffe34bf59edd
migu3langel <51764970+migu3langel@users.noreply.github.com>
2024-10-25 17:04:14 -0600

    add product rest
    Co-Authored-By: fgonzalez1010 <186346803+fgonzalez1010@users.noreply.github.com>

--- bd/src/main/java/com/proyecto/bd/services/ProductService.java
+++ bd/src/main/java/com/proyecto/bd/services/ProductService.java
@@ -1,5 +1,26 @@
 package com.proyecto.bd.services;
 
+import java.util.List;
+
+import com.proyecto.bd.entities.Product;
+import com.proyecto.bd.entities.UserClient;
+import com.proyecto.bd.repository.ProductRepository;
+
 public class ProductService {
 
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
 }
