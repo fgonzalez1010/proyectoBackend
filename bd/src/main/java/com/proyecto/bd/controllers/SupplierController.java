28c2a67e60c81ea50b715bc5af07aa998c8b8108
Hgiio-spi12 <g77hespi@gmail.com>
2024-10-25 20:37:46 -0600

    controller, service

--- bd/src/main/java/com/proyecto/bd/controllers/SupplierController.java
+++ bd/src/main/java/com/proyecto/bd/controllers/SupplierController.java
@@ -1,5 +1,71 @@
 package com.proyecto.bd.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.proyecto.bd.entities.Supplier;
+import com.proyecto.bd.services.SupplierService;
+
+@RestController
+@RequestMapping("/supplier")
 public class SupplierController {
+
+
+    @Autowired
+    private SupplierService supplierService;
+    
+
+
+   
+    @GetMapping
+    public List<Supplier> getAllSupplier() {
+        return supplierService.getAllSupplier();
+    }
+
+      @GetMapping("/{id}")
+    public ResponseEntity<Supplier> getSupplierById(@PathVariable Long id) {
+        Optional<Supplier> supplier = supplierService.getSupplierById(id);
+        return supplier.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+     @PostMapping
+    public ResponseEntity<Supplier> createSupplier(@RequestBody Supplier supplier) {
+        Supplier createdSupplier = supplierService.createSupplier(supplier);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdSupplier);
+    }
+
+      @PutMapping("/{id}")
+    public ResponseEntity<Supplier> updateSupplier(@PathVariable Long id, @RequestBody Supplier updatedSupplier) {
+        try {
+            Supplier supplier = supplierService.updateSupplier(id, updatedSupplier);
+            return ResponseEntity.ok(supplier);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+     @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSupplier(@PathVariable Long id) {
+        try {
+            supplierService.deleteSupplier(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
 
 }
--- bd/src/main/java/com/proyecto/bd/services/SupplierService.java
+++ bd/src/main/java/com/proyecto/bd/services/SupplierService.java
@@ -1,5 +1,57 @@
 package com.proyecto.bd.services;
 
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+
+import com.proyecto.bd.entities.Supplier;
+import com.proyecto.bd.repository.SupplierRepository;
+
+@Service
 public class SupplierService {
+
+    private final SupplierRepository supplierRepository;
+
+    public SupplierService(SupplierRepository supplierRepository){
+        this.supplierRepository = supplierRepository;
+    }
+
+
+    
+     public List<Supplier> getAllSupplier() {
+        return supplierRepository.findAll();
+    }
+
+    public Optional<Supplier> getSupplierById(Long id) {
+        return supplierRepository.findById(id);
+    }
+
+     public Supplier createSupplier(Supplier supplier) {
+        return supplierRepository.save(supplier);
+    }
+
+    public Supplier updateSupplier(Long id, Supplier updatedSupplier) {
+        return supplierRepository.findById(id)
+                .map(existingSupplier -> {
+                    existingSupplier.setName(updatedSupplier.getName());
+                    return supplierRepository.save(existingSupplier);
+                }).orElseThrow(() -> new RuntimeException("Supplier not found with id: " + id));
+    }
+
+    public void deleteSupplier(Long id) {
+        if (supplierRepository.existsById(id)) {
+            supplierRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("supplier not found with id: " + id);
+        }
+    }
+
+
+
+
 
 }
